fead5a5d9d47e8e47af5b7a12829cb6f4b2ffd20
Carlos Diez <cdiezmoran@gmail.com>
2017-01-31 17:30:51 -0800

    Fixed ui added some js

---
+++ app.js
@@ -0,0 +1,44 @@
+$(function () {
+  function updateSignUpButton() {
+    $btn = $('.btn-signup')
+
+    if ($btn.hasClass('green')) {
+      $btn.removeClass('green');
+      $btn.text('Sign up to build awesome games')
+      return
+    }
+
+    $btn.addClass('green');
+    $btn.text('Sign up to play awesome games')
+  }
+
+  $('.dev-select').click(function (e) {
+    e.preventDefault();
+    var $target = $('.dev-select');
+    var $playerSelect = $('.player-select');
+
+    if ($target.hasClass('active')) {
+      return
+    }
+
+    $target.addClass('active');
+    $playerSelect.removeClass('active');
+
+    updateSignUpButton();
+  })
+
+  $('.player-select').click(function (e) {
+    e.preventDefault();
+    var $target = $('.player-select');
+    var $devSelect = $('.dev-select');
+
+    if ($target.hasClass('active')) {
+      return
+    }
+
+    $target.addClass('active');
+    $devSelect.removeClass('active');
+
+    updateSignUpButton();
+  })
+})
